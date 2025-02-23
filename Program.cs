bd689009c785f0f56328048f1c6ee705d8733473
m.alekseev <m.alekseev@nexters.com>
2025-02-23 20:27:10 +0300

    updeted hook

--- Program.cs
+++ Program.cs
@@ -1,28 +1,48 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using TelegramBot.Services;
 
-var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
-    {
-       // Конфигурация бота
-       services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7996792416:AAFGNZkdgemeWxqT7_JMVtsJxiIWf3TyQow"));
-       // Регистрация сервисов
-       services.AddScoped<IUpdateHandler, UpdateHandler>();
-       services.AddHostedService<BotService>();
 
-       // Настройка логирования
-       services.AddLogging(logging =>
-       {
-          logging.ClearProviders();
-          logging.AddConsole();
-          logging.SetMinimumLevel(LogLevel.Debug);
-       });
-    })
-    .Build();
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddHostedService<BotService>();
+//var app = builder.Build();
 
-await host.RunAsync();
-//await Task.Delay(-1);
+//app.MapGet("/", () => "Bot is running!");
+//app.Run();
+
+//var host = Host.CreateDefaultBuilder(args)
+//    .ConfigureServices((context, services) =>
+//    {
+//       // Конфигурация бота
+//       services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7996792416:AAFGNZkdgemeWxqT7_JMVtsJxiIWf3TyQow"));
+//       // Регистрация сервисов
+//       services.AddScoped<IUpdateHandler, UpdateHandler>();
+//       services.AddHostedService<BotService>();
+
+//       // Настройка логирования
+//       services.AddLogging(logging =>
+//       {
+//          logging.ClearProviders();
+//          logging.AddConsole();
+//          logging.SetMinimumLevel(LogLevel.Debug);
+//       });
+//    })
+//    .Build();
+
+//await host.RunAsync();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddSingleton<ITelegramBotClient>(_ =>
+        new TelegramBotClient("7996792416:AAFGNZkdgemeWxqT7_JMVtsJxiIWf3TyQow"))
+    .AddScoped<IUpdateHandler, UpdateHandler>()
+    .AddHostedService<BotService>();
+
+var app = builder.Build();
+app.MapGet("/", () => "Telegram Bot is running!");
+app.Run();
